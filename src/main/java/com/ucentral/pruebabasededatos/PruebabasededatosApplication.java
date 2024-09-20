d2b665b6f49d054b2fd4ce59e2c20b9266b8acce
Luis <luis@example.com>
2024-09-19 19:24:15 -0500

    (Nueva): configuracion del proyecto con springboot

---
+++ src/main/java/com/ucentral/pruebabasededatos/PruebabasededatosApplication.java
@@ -0,0 +1,13 @@
+package com.ucentral.pruebabasededatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebabasededatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebabasededatosApplication.class, args);
+	}
+
+}
